46fc52aea4bc1fccdebd6531cf3c042208649ce3
chunji86 <you@example.com>
2025-08-12 13:31:06 +0900

    Add Cafe24 latest routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
